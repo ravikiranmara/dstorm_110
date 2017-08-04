d883b3cd277e512a945969f74de549cd18e98e3e
ravikiranmara <ravikiran.mara@gmail.com>
2017-08-03 23:32:33 -0500

    vanilla - builds, yet to test

---
+++ storm-core/src/jvm/org/apache/storm/scheduler/elasticity/ElasticityScheduler.java
@@ -0,0 +1,29 @@
+package org.apache.storm.scheduler.elasticity;
+
+import org.apache.storm.scheduler.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ElasticityScheduler implements IScheduler {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ElasticityScheduler.class);
+	static int flag = 0; // Todo : remove this later. this is to make sure we do scale out only once
+	@SuppressWarnings("rawtypes")
+	private Map _conf;
+
+	@Override
+	public void prepare(@SuppressWarnings("rawtypes") Map conf) {
+		_conf = conf;
+	}
+
+	@Override
+	public void schedule(Topologies topologies, Cluster cluster) {
+		LOG.info("\n\n\nRerunning ElasticityScheduler...");
+	}
+}
+
